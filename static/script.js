3f4eaee5d8a20b41adf10c5b807d3df9ad432b1e
franciscocezar <franciscocezar.jr@outlook.com>
2022-09-26 10:27:29 -0300

    animacao parte um

--- static/script.js
+++ static/script.js
@@ -20,3 +20,25 @@ navItem.forEach(item => {
         };
     });
 });
+
+/* Animar */
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+    
+    item.forEach(element => {
+        if (windowTop > element.offserTop){
+            element.classList.add("animate");
+        }else {
+            element.classList.remove("animate");
+        }
+    });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", () =>{
+    animeScroll();
+});
