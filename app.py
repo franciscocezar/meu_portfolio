3e37d98eb2f778601cba1b3928ed923c6aac1f3d
franciscocezar <franciscocezar.jr@outlook.com>
2022-09-27 14:23:44 -0300

    atualizacoes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.secret_key = 'vlksdv0w9g09g4gkmwr,vw-e2///z-0+%#4asld'
 
 @app.route('/')
 def index():
@@ -8,3 +10,5 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
